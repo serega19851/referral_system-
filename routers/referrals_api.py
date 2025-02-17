cc9f3dbb4db6c4cecca467809c7e215018104747
sergeyyyy1985.an@gmail.com <sergeyyyy1985.an@gmail.com>
2025-02-17 20:23:54 +0300

    Fix

--- routers/referrals_api.py
+++ routers/referrals_api.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException, Path
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth import verify_token
@@ -10,12 +9,15 @@ from crudd.referral_crud import (
     get_referrals,
 )
 from database import get_async_session
-from models import User
 
 referrals_router = APIRouter()
 
 
-@referrals_router.post("/referrals/code/", tags=["Referrals"], description="Создает новый реферальный код если его нет или если он уже устарел по времени")
+@referrals_router.post(
+    "/referrals/code/",
+    tags=["Referrals"],
+    description="Создает новый реферальный код если его нет или если он уже устарел по времени",
+)
 async def create_referral_code(
     payload: dict = Depends(verify_token), db: AsyncSession = Depends(get_async_session)
 ):
@@ -23,7 +25,11 @@ async def create_referral_code(
     return f"{referral_codes} "
 
 
-@referrals_router.delete("/referrals/code/", tags=["Referrals"], description="Удаляет реферальный код пользователя" )
+@referrals_router.delete(
+    "/referrals/code/",
+    tags=["Referrals"],
+    description="Удаляет реферальный код пользователя",
+)
 async def delete_referral_code(
     payload: dict = Depends(verify_token), db: AsyncSession = Depends(get_async_session)
 ):
@@ -31,13 +37,16 @@ async def delete_referral_code(
     return f"{dell_ref} "
 
 
-@referrals_router.get("/referrals/code/", tags=["Referrals"], description="Получает свой реферальный код по своему емаил")
+@referrals_router.get(
+    "/referrals/code/",
+    tags=["Referrals"],
+    description="Получает свой реферальный код по своему емаил",
+)
 async def get_referral(
     email: str,
     payload: dict = Depends(verify_token),
     db: AsyncSession = Depends(get_async_session),
 ):
-    # Проверяем, что email из запроса совпадает с email в токене
     if email != payload.get("email"):
         raise HTTPException(
             status_code=403, detail="Нет доступа к данным другого пользователя"
@@ -49,19 +58,6 @@ async def get_referral(
         return {"referral_code": referral_code}
     else:
         raise HTTPException(status_code=404, detail="Реферальный код не найден")
-
-
-# @referrals_router.get("/referrals/code/", tags=["Referrals"])
-# async def get_referral(
-#     email: str,
-#     payload: dict = Depends(verify_token),
-#     db: AsyncSession = Depends(get_async_session),
-# ):
-#     referral_code = await get_referral_code_by_email(db, email)
-#     if referral_code:
-#         return {"referral_code": referral_code}
-#     else:
-#         raise HTTPException(status_code=404, detail="Реферальный код не найден")
 
 
 @referrals_router.get("/users/{user_id}/referrals", tags=["Referrals"])
@@ -79,29 +75,3 @@ async def get_user_referrals(
 
     referrals = await get_referrals(db, user_id)
     return referrals
-
-
-# @referrals_router.get("/users/{user_id}/referrals", tags=["Referrals"])
-# async def get_user_referrals(
-#     user_id: int, db: AsyncSession = Depends(get_async_session)
-# ):
-#     referrals = await get_referrals(db, user_id)
-#     return referrals
-
-
-######################################################
-async def delete_all_users(db: AsyncSession):
-    async with db.begin():
-        result = await db.execute(select(User))
-        users = result.scalars().all()
-
-        for user in users:
-            await db.delete(user)
-
-    await db.commit()
-
-
-@referrals_router.delete("/users/delete/")
-async def delete_referral_code(db: AsyncSession = Depends(get_async_session)):
-    await delete_all_users(db)
-    return {"message": "All users have been deleted"}
--- run.sh
+++ run.sh
@@ -12,9 +12,31 @@ source venv/bin/activate
 echo "Установка зависимостей..."
 pip install --no-cache-dir -r requirements.txt
 
+echo "Проверка инициализации Alembic..."
+if [ ! -d "alembic" ]; then
+    echo "Alembic не инициализирован. Инициализирую..."
+    alembic init alembic
+fi
+
+echo "Настройка alembic.ini..."
+sed -i 's|^sqlalchemy.url =.*|sqlalchemy.url = sqlite:///./sqlite.db|' alembic.ini
+
+echo "Редактирование alembic/env.py..."
+sed -i 's|import sys|import sys\nimport os\nsys.path.append(os.getcwd())|' alembic/env.py
+sed -i 's|# add your model.*|from database import Base\nfrom models import User, ReferralCode|' alembic/env.py
+sed -i 's|target_metadata = .*|target_metadata = Base.metadata|' alembic/env.py
+
+
+echo "Проверка папки версий Alembic..."
+if [ -z "$(ls -A alembic/versions 2>/dev/null)" ]; then
+    echo "Создание первой миграции..."
+    alembic revision --autogenerate -m "initial migration"
+fi
+
 echo "Применение миграций..."
 alembic upgrade head
 
+echo "Скрипт выполнен успешно."
 echo "Запуск проекта..."
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 
